fe2a919b933741530e93d9571a855b24d9b7b48b
mathieuportal <mathieuportal06@gmail.com>
2020-10-13 13:43:53 +0200

    Finalized back

--- src/com/inti/entities/Destination.java
+++ src/com/inti/entities/Destination.java
@@ -77,4 +77,31 @@ public class Destination implements Serializable {
 		this.hotels = hotels;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Destination other = (Destination) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+
 }
